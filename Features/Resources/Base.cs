b759b86ae907d9309bfa2e9c0963b909bba9092c
Exanite <exanite78012@gmail.com>
2023-11-10 15:43:52 -0500

    Add tile definitions

--- Features/Resources/Base.cs
+++ Features/Resources/Base.cs
@@ -5,6 +5,9 @@ namespace Exanite.Extraction.Features.Resources;
 
 public static class Base
 {
-    public static PropertyDefinition<Texture2D> Player = new("Base:Player.png");
-    public static PropertyDefinition<Texture2D> Wall = new("Base:Wall.png");
+    public static PropertyDefinition<Texture2D> Tile1 = new("Base:Tile1.png");
+    public static PropertyDefinition<Texture2D> Tile2 = new("Base:Tile2.png");
+    public static PropertyDefinition<Texture2D> Tile3 = new("Base:Tile3.png");
+    public static PropertyDefinition<Texture2D> Tile4 = new("Base:Tile4.png");
+    public static PropertyDefinition<Texture2D> Tile5 = new("Base:Tile5.png");
 }
