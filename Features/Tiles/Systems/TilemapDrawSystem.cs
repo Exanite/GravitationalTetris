82811e0503454a034bd9c2c1d54c0dcf7dd2a011
Exanite <exanite78012@gmail.com>
2023-11-11 16:46:22 -0500

    Implement block position rotations

--- Features/Tiles/Systems/TilemapDrawSystem.cs
+++ Features/Tiles/Systems/TilemapDrawSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Arch.System;
 using Arch.System.SourceGenerator;
 using Exanite.Core.Utilities;
@@ -7,6 +8,7 @@ using Exanite.WarGames.Features.Cameras.Components;
 using Exanite.WarGames.Features.Resources;
 using Exanite.WarGames.Features.Sprites;
 using Exanite.WarGames.Features.Time;
+using Exanite.WarGames.Features.Transforms.Components;
 using Exanite.WarGames.Systems;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -39,12 +41,13 @@ public partial class TilemapDrawSystem : EcsSystem, IDrawSystem, ICallbackSystem
 
     public void Draw()
     {
-        DrawQuery(World);
+        DrawTilesQuery(World);
+        DrawPlaceholdersQuery(World);
     }
 
     [Query]
     [All<CameraComponent>]
-    private void Draw(ref CameraProjectionComponent cameraProjection)
+    private void DrawTiles(ref CameraProjectionComponent cameraProjection)
     {
         var spriteBatch = gameSpriteBatch.SpriteBatch;
 
@@ -83,6 +86,57 @@ public partial class TilemapDrawSystem : EcsSystem, IDrawSystem, ICallbackSystem
             }
         }
         spriteBatch.End();
+    }
+
+    [Query]
+    private void DrawPlaceholders(ref TetrisRootComponent root, ref TransformComponent transform)
+    {
+        DrawPlaceholders_1Query(World, ref root, ref transform);
+    }
+
+    public record struct Position(int X, int Y);
+
+    private readonly List<Position> blockPositions = new();
+
+    [Query]
+    [All<CameraComponent>]
+    private void DrawPlaceholders_1([Data] ref TetrisRootComponent tetrisRoot, [Data] ref TransformComponent tetrisTransform, ref CameraProjectionComponent cameraProjection)
+    {
+        var spriteBatch = gameSpriteBatch.SpriteBatch;
+
+        var predictedX = (int)MathF.Round(tetrisTransform.Position.X);
+        var predictedY = (int)MathF.Ceiling(tetrisTransform.Position.Y);
+
+        blockPositions.Clear();
+        for (var x = 0; x < tetrisRoot.Definition.Shape.GetLength(0); x++)
+        {
+            for (var y = 0; y < tetrisRoot.Definition.Shape.GetLength(1); y++)
+            {
+                if (!tetrisRoot.Definition.Shape[x, y])
+                {
+                    continue;
+                }
+
+                var position = new Position(x - tetrisRoot.Definition.PivotX, y - tetrisRoot.Definition.PivotY);
+                for (var i = 0; i < (int)tetrisRoot.Rotation; i++)
+                {
+                    position = new Position(-position.Y, position.X);
+                }
+
+                position.X += predictedX;
+                position.Y += predictedY;
+
+                blockPositions.Add(position);
+            }
+        }
+
+        // Calculate predictedY
+        while (true)
+        {
+            // tetrisRoot.Definition
+
+            break;
+        }
 
         spriteBatch.Begin(
             SpriteSortMode.Deferred,
@@ -94,31 +148,29 @@ public partial class TilemapDrawSystem : EcsSystem, IDrawSystem, ICallbackSystem
             cameraProjection.WorldToScreen
         );
         {
-            for (var x = 0; x < tilemap.Tiles.GetLength(0); x++)
+            foreach (var blockPosition in blockPositions)
             {
-                for (var y = 0; y < tilemap.Tiles.GetLength(1); y++)
-                {
-                    var sprite = placeholderTileTexture.Value;
+                var sprite = placeholderTileTexture.Value;
 
-                    var maxAlpha = 0.8f;
-                    var minAlpha = 0.1f;
-                    var alpha = MathUtility.Remap(EaseInOutCubic(time.Time / 1.5f), 0, 1, minAlpha, maxAlpha);
+                var maxAlpha = 0.8f;
+                var minAlpha = 0.1f;
+                var alpha = MathUtility.Remap(EaseInOutCubic(time.Time / 1.5f), 0, 1, minAlpha, maxAlpha);
 
-                    var sourceRect = new Rectangle(0, 0, sprite.Width, sprite.Height);
-                    var origin = new Vector2(0.5f * sprite.Width, 0.5f * sprite.Height);
-                    var scale = new Vector2(1f / sprite.Width, 1f / sprite.Height);
+                var sourceRect = new Rectangle(0, 0, sprite.Width, sprite.Height);
+                var origin = new Vector2(0.5f * sprite.Width, 0.5f * sprite.Height);
+                var scale = new Vector2(1f / sprite.Width, 1f / sprite.Height);
+                var position = new Vector2(blockPosition.X, blockPosition.Y);
 
-                    spriteBatch.Draw(
-                        sprite, // Texture
-                        new Vector2(x, y), // Position
-                        sourceRect, // Source Rect
-                        new Color(1, 1, 1, alpha), // Color
-                        0, // Rotation
-                        origin, // Origin
-                        scale, // Scale
-                        SpriteEffects.None, // Effects
-                        0); // Depth
-                }
+                spriteBatch.Draw(
+                    sprite, // Texture
+                    position, // Position
+                    sourceRect, // Source Rect
+                    new Color(1, 1, 1, alpha), // Color
+                    0, // Rotation
+                    origin, // Origin
+                    scale, // Scale
+                    SpriteEffects.None, // Effects
+                    0); // Depth
             }
         }
         spriteBatch.End();
