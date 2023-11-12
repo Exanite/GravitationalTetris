71de10a746df928310492db8b8444a0839ea1108
Exanite <exanite78012@gmail.com>
2023-11-12 12:47:56 -0500

    Implement simple UI

--- Features/Tiles/Systems/TetrisUiSystem.cs
+++ Features/Tiles/Systems/TetrisUiSystem.cs
@@ -1,7 +1,10 @@
 using Exanite.ResourceManagement;
 using Exanite.WarGames.Systems;
+using FontStashSharp.RichText;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Myra;
+using Myra.Graphics2D.Brushes;
 using Myra.Graphics2D.UI;
 
 namespace Exanite.WarGames.Features.Tiles.Systems;
@@ -28,69 +31,55 @@ public class TetrisUiSystem : EcsSystem, IStartSystem, IUpdateSystem, IDrawSyste
         DefaultAssets.AssetManager = assetManager;
 
         desktop = new Desktop();
+    }
 
-        var grid = new Grid
+    public void Update()
+    {
+        var mainGrid = new Grid
         {
             RowSpacing = 8,
             ColumnSpacing = 8,
         };
 
-        grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-        grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+        mainGrid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+        mainGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+        mainGrid.RowsProportions.Add(new Proportion(ProportionType.Auto));
 
-        var helloWorld = new Label
         {
-            Id = "label",
-            Text = "Hello, World!",
-        };
-        grid.Widgets.Add(helloWorld);
-
-        // ComboBox
-        var combo = new ComboBox();
-        Grid.SetColumn(combo, 1);
-        Grid.SetRow(combo, 0);
-
-        combo.Items.Add(new ListItem("Red", Color.Red));
-        combo.Items.Add(new ListItem("Green", Color.Green));
-        combo.Items.Add(new ListItem("Blue", Color.Blue));
-        grid.Widgets.Add(combo);
-
-        // Button
-        var button = new Button
-        {
-            Content = new Label
+            var score = new Label
             {
-                Text = "Show",
-            },
-        };
-        Grid.SetColumn(button, 0);
-        Grid.SetRow(button, 1);
+                Text = $"Score: {12345}\n",
+            };
 
-        button.Click += (s, a) =>
+            mainGrid.Widgets.Add(score);
+            Grid.SetRow(score, 0);
+            Grid.SetColumn(score, 0);
+        }
+
         {
-            var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
-            messageBox.ShowModal(desktop);
-        };
+            var leaderboardTitle = new Label
+            {
+                Text = $"Leaderboard:",
+            };
 
-        grid.Widgets.Add(button);
+            mainGrid.Widgets.Add(leaderboardTitle);
+            Grid.SetRow(leaderboardTitle, 1);
+            Grid.SetColumn(leaderboardTitle, 0);
+        }
 
-        // Spin button
-        var spinButton = new SpinButton
         {
-            Width = 100,
-            Nullable = true,
-        };
-        Grid.SetColumn(spinButton, 1);
-        Grid.SetRow(spinButton, 1);
+            var leaderboardEntries = new Label
+            {
+                Text = $"1. 34534534\n1. 34534534\n1. 34534534\n1. 34534534",
+            };
 
-        grid.Widgets.Add(spinButton);
+            mainGrid.Widgets.Add(leaderboardEntries);
+            Grid.SetRow(leaderboardEntries, 2);
+            Grid.SetColumn(leaderboardEntries, 0);
+        }
 
-        desktop.Root = grid;
+        desktop.Root = mainGrid;
     }
-
-    public void Update() {}
 
     public void Draw()
     {
