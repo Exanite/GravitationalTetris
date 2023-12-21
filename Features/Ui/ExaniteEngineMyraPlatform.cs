3c4d9cc330a48f1d50842ecd833e7c3e2ffa6296
Exanite <exanite78012@gmail.com>
2023-12-21 12:32:22 -0500

    Implement DrawSprite with simple implementation for testing purposes

--- Features/Ui/ExaniteEngineMyraPlatform.cs
+++ Features/Ui/ExaniteEngineMyraPlatform.cs
@@ -4,6 +4,8 @@ using System.Drawing;
 using System.Numerics;
 using Diligent;
 using Exanite.Engine.Rendering;
+using Exanite.GravitationalTetris.Features.Rendering;
+using Exanite.GravitationalTetris.Features.Sprites.Systems;
 using FontStashSharp;
 using FontStashSharp.Interfaces;
 using Myra.Graphics2D;
@@ -59,8 +61,12 @@ public class ExaniteEngineMyraPlatform : IMyraPlatform
 
 public class ExaniteEngineMyraRenderer : IMyraRenderer
 {
-    public ExaniteEngineMyraRenderer(ITexture2DManager textureManager)
+    private readonly SpriteBatchSystem spriteBatchSystem;
+
+    public ExaniteEngineMyraRenderer(ITexture2DManager textureManager, SpriteBatchSystem spriteBatchSystem)
     {
+        this.spriteBatchSystem = spriteBatchSystem;
+
         TextureManager = textureManager;
     }
 
@@ -81,7 +87,15 @@ public class ExaniteEngineMyraRenderer : IMyraRenderer
 
     public void DrawSprite(object texture, Vector2 pos, Rectangle? src, FSColor color, float rotation, Vector2 scale, float depth)
     {
-        throw new NotImplementedException();
+        var typedTexture = (Texture2D)texture;
+
+        spriteBatchSystem.DrawSprite(typedTexture, new SpriteUniformData
+        {
+            World = Matrix4x4.Identity,
+            View = Matrix4x4.Identity,
+            Projection = Matrix4x4.Identity,
+            Color = Vector4.One,
+        });
     }
 
     public void DrawQuad(object texture, ref VertexPositionColorTexture topLeft, ref VertexPositionColorTexture topRight, ref VertexPositionColorTexture bottomLeft, ref VertexPositionColorTexture bottomRight)
