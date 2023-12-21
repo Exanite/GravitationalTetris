d29502819a0ed20db19889b47331557df3da1264
Exanite <exanite78012@gmail.com>
2023-12-21 13:37:13 -0500

    Implement UI rendering (seems to have artifacting right now)

--- Features/Ui/ExaniteEngineMyraRenderer.cs
+++ Features/Ui/ExaniteEngineMyraRenderer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Drawing;
 using System.Numerics;
 using Exanite.Engine.Rendering;
+using Exanite.Engine.Windowing;
 using Exanite.GravitationalTetris.Features.Rendering;
 using Exanite.GravitationalTetris.Features.Sprites.Systems;
 using FontStashSharp;
@@ -14,11 +15,12 @@ namespace Exanite.GravitationalTetris.Features.Ui;
 public class ExaniteEngineMyraRenderer : IMyraRenderer
 {
     private readonly SpriteBatchSystem spriteBatchSystem;
+    private readonly Window window;
 
-    public ExaniteEngineMyraRenderer(ITexture2DManager textureManager, SpriteBatchSystem spriteBatchSystem)
+    public ExaniteEngineMyraRenderer(SpriteBatchSystem spriteBatchSystem, Window window, ITexture2DManager textureManager)
     {
         this.spriteBatchSystem = spriteBatchSystem;
-
+        this.window = window;
         TextureManager = textureManager;
     }
 
@@ -41,12 +43,31 @@ public class ExaniteEngineMyraRenderer : IMyraRenderer
     {
         var typedTexture = (Texture2D)texture;
 
+        var offset = Vector2.Zero;
+        var pixelSize = new Vector2(typedTexture.Width, typedTexture.Height);
+        var size = Vector2.One;
+        if (src.HasValue)
+        {
+            var rect = src.Value;
+            offset = new Vector2((float)rect.X / typedTexture.Width, (float)rect.Y / typedTexture.Height);
+            pixelSize = new Vector2(rect.Width, rect.Height);
+            size = new Vector2((float)rect.Width / typedTexture.Width, (float)rect.Height / typedTexture.Height);
+        }
+
+        var world = Matrix4x4.CreateScale(scale.X * pixelSize.X, scale.Y * pixelSize.Y, 1) * Matrix4x4.CreateRotationZ(rotation) * Matrix4x4.CreateTranslation(pos.X, pos.Y, 0);
+        var view = Matrix4x4.CreateTranslation(0, 0, -10);
+        var projection = Matrix4x4.CreateOrthographicOffCenter(0, window.Settings.Width, 0, window.Settings.Height, 0.001f, 1000f) * Matrix4x4.CreateRotationZ(float.Pi) * Matrix4x4.CreateScale(-1, 1, 1);
+
         spriteBatchSystem.DrawSprite(typedTexture, new SpriteUniformData
         {
-            World = Matrix4x4.Identity,
-            View = Matrix4x4.Identity,
-            Projection = Matrix4x4.Identity,
-            Color = Vector4.One,
+            World = world,
+            View = view,
+            Projection = projection,
+
+            Color = new Vector4(color.R / 255f, color.G / 255f, color.B / 255f, color.A / 255f),
+
+            Offset = offset,
+            Size = size,
         });
     }
 
