2d3ee1175bfb8df77f805ef8edb8a0ebfedf14bc
Exanite <exanite78012@gmail.com>
2023-12-21 12:22:45 -0500

    Mark all SpriteUniformData fields as required

--- Features/Rendering/SpriteUniformData.cs
+++ Features/Rendering/SpriteUniformData.cs
@@ -6,9 +6,9 @@ namespace Exanite.GravitationalTetris.Features.Rendering;
 [StructLayout(LayoutKind.Sequential)]
 public struct SpriteUniformData
 {
-    public Matrix4x4 World;
-    public Matrix4x4 View;
-    public Matrix4x4 Projection;
+    public required Matrix4x4 World;
+    public required Matrix4x4 View;
+    public required Matrix4x4 Projection;
 
-    public Vector4 Color;
+    public required Vector4 Color;
 }
