e9b2e37275beacc88fd390284d0597c3455c09ff
Exanite <exanite78012@gmail.com>
2023-12-24 16:36:47 -0500

    Remove Texture field from SpriteUniformData

--- Features/Sprites/SpriteUniformData.cs
+++ Features/Sprites/SpriteUniformData.cs
@@ -1,14 +1,12 @@
 using System.Numerics;
 using System.Runtime.InteropServices;
-using Exanite.Engine.Rendering;
+using Diligent;
 
 namespace Exanite.GravitationalTetris.Features.Sprites;
 
 [StructLayout(LayoutKind.Sequential)]
 public struct SpriteUniformData
 {
-    public required Texture2D Texture;
-
     public required Matrix4x4 View;
     public required Matrix4x4 Projection;
 }
