dfd8e2b77f8001fbb6141d7f888349da1987eede
Exanite <exanite78012@gmail.com>
2023-12-24 16:36:15 -0500

    Add SpriteInstanceData layout

--- Features/Sprites/SpriteInstanceData.cs
+++ Features/Sprites/SpriteInstanceData.cs
@@ -1,12 +1,68 @@
 using System.Numerics;
 using System.Runtime.InteropServices;
+using Diligent;
 
 namespace Exanite.GravitationalTetris.Features.Sprites;
 
 [StructLayout(LayoutKind.Sequential)]
 public struct SpriteInstanceData
 {
-    public required Matrix4x4 WorldViewProjection;
+    public static InputLayoutDesc Layout { get; } = new InputLayoutDesc
+    {
+        LayoutElements = new LayoutElement[]
+        {
+            new LayoutElement
+            {
+                HLSLSemantic = "ATTRIB",
+                InputIndex = 0,
+
+                NumComponents = 16,
+                ValueType = ValueType.Float32,
+
+                IsNormalized = false,
+
+                Frequency = InputElementFrequency.PerInstance,
+            },
+            new LayoutElement
+            {
+                HLSLSemantic = "ATTRIB",
+                InputIndex = 1,
+
+                NumComponents = 4,
+                ValueType = ValueType.Float32,
+
+                IsNormalized = false,
+
+                Frequency = InputElementFrequency.PerInstance,
+            },
+            new LayoutElement
+            {
+                HLSLSemantic = "ATTRIB",
+                InputIndex = 2,
+
+                NumComponents = 2,
+                ValueType = ValueType.Float32,
+
+                IsNormalized = false,
+
+                Frequency = InputElementFrequency.PerInstance,
+            },
+            new LayoutElement
+            {
+                HLSLSemantic = "ATTRIB",
+                InputIndex = 3,
+
+                NumComponents = 2,
+                ValueType = ValueType.Float32,
+
+                IsNormalized = false,
+
+                Frequency = InputElementFrequency.PerInstance,
+            },
+        },
+    };
+
+    public required Matrix4x4 World;
 
     public Vector4 Color;
 
