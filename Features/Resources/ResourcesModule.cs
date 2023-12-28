2f13998f8c9884bd3d731e44aeb67bde77e57032
Exanite <exanite78012@gmail.com>
2023-12-28 17:55:44 -0500

    Add Lighting content to ResourceManager

--- Features/Resources/ResourcesModule.cs
+++ Features/Resources/ResourcesModule.cs
@@ -40,9 +40,13 @@ public class ResourcesModule : Module
                 var rendererContext = e.Context.Resolve<RendererContext>();
 
                 resourceManager.Mount("Base:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Base")), true);
+
                 resourceManager.Mount("Myra:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Myra")), true);
+
                 resourceManager.Mount("Winter:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Winter", "Content")), true);
                 resourceManager.Mount("Base:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Winter", "Overrides", "Base")), true);
+
+                resourceManager.Mount("Lighting:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Lighting")), true);
 
                 resourceManager.RegisterLoader<Shader>(loadOperation =>
                 {
