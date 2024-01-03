703ffeac74bca47247e19b5b2cbf00db2f7e3c6d
Exanite <exanite78012@gmail.com>
2024-01-02 21:29:56 -0500

    Add start of bloom shaders

---
+++ Content/Rendering/Bloom.v.hlsl
@@ -0,0 +1,24 @@
+struct Input
+{
+    uint VertexId : SV_VertexID;
+};
+
+struct Output
+{
+    float4 Pos : SV_POSITION;
+    float2 Uv : TEX_COORD;
+};
+
+void main(
+    in Input input,
+    out Output output)
+{
+    float4 positionUvs[4];
+    positionUvs[0] = float4(-1, 1, 0, 1);
+    positionUvs[1] = float4(-1, -1, 0, 0);
+    positionUvs[2] = float4(1, 1, 1, 1);
+    positionUvs[3] = float4(1, -1, 1, 0);
+
+    output.Pos = float4(positionUvs[input.VertexId].xy, 0, 1);
+    output.Uv = float2(positionUvs[input.VertexId].zw);
+}
---
+++ Content/Rendering/BloomDown.p.hlsl
@@ -0,0 +1,20 @@
+Texture2D Texture;
+SamplerState TextureSampler;
+
+struct Input
+{
+    float4 Pos : SV_POSITION;
+    float2 Uv : TEX_COORD;
+};
+
+struct Output
+{
+    float4 Color : SV_TARGET;
+};
+
+void main(
+    in Input input,
+    out Output output)
+{
+    output.Color = Texture.Sample(TextureSampler, input.Uv);
+}
