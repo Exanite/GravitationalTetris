10419e7fd08713b486876c97656c5d8b15208b5e
Exanite <exanite78012@gmail.com>
2024-01-03 16:46:51 -0500

    Add BlendDesc to bloom up pipeline

--- Features/Rendering/Systems/BloomSystem.cs
+++ Features/Rendering/Systems/BloomSystem.cs
@@ -148,6 +148,19 @@ public class BloomSystem : ISetupSystem, IRenderSystem, ITeardownSystem
 
                     RasterizerDesc = new RasterizerStateDesc { CullMode = CullMode.None },
                     DepthStencilDesc = new DepthStencilStateDesc { DepthEnable = false },
+
+                    BlendDesc = new BlendStateDesc
+                    {
+                        RenderTargets = new RenderTargetBlendDesc[]
+                        {
+                            new()
+                            {
+                                BlendEnable = true,
+                                SrcBlend = BlendFactor.SrcAlpha,
+                                DestBlend = BlendFactor.InvSrcAlpha,
+                            },
+                        },
+                    },
                 },
 
                 Vs = vShader.Value.Handle,
