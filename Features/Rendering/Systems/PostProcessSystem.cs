b213976cc689bbba5b5e18e3a54571aa678adc97
Exanite <exanite78012@gmail.com>
2024-01-14 00:03:34 -0500

    Implement hot reloading for PostProcessSystem shader pipeline

--- Features/Rendering/Systems/PostProcessSystem.cs
+++ Features/Rendering/Systems/PostProcessSystem.cs
@@ -17,7 +17,7 @@ public class PostProcessSystem : ISetupSystem, IRenderSystem, ITeardownSystem
 {
     private Buffer<PostProcessUniformData> uniformBuffer = null!;
     private ISampler textureSampler = null!;
-    private IPipelineState pipeline = null!;
+    private Reloadable<IPipelineState> pipeline = null!;
     private IShaderResourceBinding shaderResourceBinding = null!;
     private IShaderResourceVariable? textureVariable;
 
@@ -39,7 +39,6 @@ public class PostProcessSystem : ISetupSystem, IRenderSystem, ITeardownSystem
     public void Setup()
     {
         var renderDevice = rendererContext.RenderDevice;
-        var swapChain = rendererContext.SwapChain;
 
         var vShader = resourceManager.GetResource<Shader>("Rendering:Screen.v.hlsl");
         var pShader = resourceManager.GetResource<Shader>("Rendering:PostProcess.p.hlsl");
@@ -57,45 +56,51 @@ public class PostProcessSystem : ISetupSystem, IRenderSystem, ITeardownSystem
             AddressU = TextureAddressMode.Mirror, AddressV = TextureAddressMode.Mirror, AddressW = TextureAddressMode.Mirror,
         });
 
-        pipeline = renderDevice.CreateGraphicsPipelineState(new GraphicsPipelineStateCreateInfo
+        pipeline = new Reloadable<IPipelineState>(dependencies =>
         {
-            PSODesc = new PipelineStateDesc
+            dependencies.Add(vShader);
+            dependencies.Add(pShader);
+
+            return renderDevice.CreateGraphicsPipelineState(new GraphicsPipelineStateCreateInfo
             {
-                Name = "Post Process Shader Pipeline",
-                ResourceLayout = new PipelineResourceLayoutDesc
+                PSODesc = new PipelineStateDesc
                 {
-                    DefaultVariableType = ShaderResourceVariableType.Static,
-                    Variables = new ShaderResourceVariableDesc[]
+                    Name = "Post Process Shader Pipeline",
+                    ResourceLayout = new PipelineResourceLayoutDesc
                     {
-                        new()
+                        DefaultVariableType = ShaderResourceVariableType.Static,
+                        Variables = new ShaderResourceVariableDesc[]
                         {
-                            ShaderStages = ShaderType.Pixel,
-                            Name = "Texture",
-                            Type = ShaderResourceVariableType.Mutable,
+                            new()
+                            {
+                                ShaderStages = ShaderType.Pixel,
+                                Name = "Texture",
+                                Type = ShaderResourceVariableType.Mutable,
+                            },
                         },
                     },
                 },
-            },
 
-            GraphicsPipeline = new GraphicsPipelineDesc
-            {
-                PrimitiveTopology = PrimitiveTopology.TriangleStrip,
+                GraphicsPipeline = new GraphicsPipelineDesc
+                {
+                    PrimitiveTopology = PrimitiveTopology.TriangleStrip,
 
-                NumRenderTargets = 1,
-                RTVFormats = new[] { TextureFormat.RGB32_Float },
+                    NumRenderTargets = 1,
+                    RTVFormats = new[] { TextureFormat.RGB32_Float },
 
-                RasterizerDesc = new RasterizerStateDesc { CullMode = CullMode.None },
-                DepthStencilDesc = new DepthStencilStateDesc { DepthEnable = false },
-            },
+                    RasterizerDesc = new RasterizerStateDesc { CullMode = CullMode.None },
+                    DepthStencilDesc = new DepthStencilStateDesc { DepthEnable = false },
+                },
 
-            Vs = vShader.Value.Handle,
-            Ps = pShader.Value.Handle,
+                Vs = vShader.Value.Handle,
+                Ps = pShader.Value.Handle,
+            });
         });
 
-        pipeline.GetStaticVariableByName(ShaderType.Pixel, "TextureSampler")?.Set(textureSampler, SetShaderResourceFlags.None);
-        pipeline.GetStaticVariableByName(ShaderType.Pixel, "Uniforms")?.Set(uniformBuffer.Handle, SetShaderResourceFlags.None);
+        pipeline.Value.GetStaticVariableByName(ShaderType.Pixel, "TextureSampler")?.Set(textureSampler, SetShaderResourceFlags.None);
+        pipeline.Value.GetStaticVariableByName(ShaderType.Pixel, "Uniforms")?.Set(uniformBuffer.Handle, SetShaderResourceFlags.None);
 
-        shaderResourceBinding = pipeline.CreateShaderResourceBinding(true);
+        shaderResourceBinding = pipeline.Value.CreateShaderResourceBinding(true);
         textureVariable = shaderResourceBinding.GetVariableByName(ShaderType.Pixel, "Texture");
     }
 
@@ -116,7 +121,7 @@ public class PostProcessSystem : ISetupSystem, IRenderSystem, ITeardownSystem
 
         textureVariable?.Set(worldRenderTextureSystem.WorldColorView, SetShaderResourceFlags.AllowOverwrite);
 
-        deviceContext.SetPipelineState(pipeline);
+        deviceContext.SetPipelineState(pipeline.Value);
         deviceContext.CommitShaderResources(shaderResourceBinding, ResourceStateTransitionMode.Transition);
         deviceContext.Draw(new DrawAttribs
         {
