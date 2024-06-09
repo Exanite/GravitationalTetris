e5bf116b93e17b612fb2f9264edb62c57e36b5db
Exanite <exanite78012@gmail.com>
2024-06-09 02:54:39 -0400

    Cleanup using statements

--- Features/Resources/ResourcesModule.cs
+++ Features/Resources/ResourcesModule.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 using Autofac;
 using Diligent;
