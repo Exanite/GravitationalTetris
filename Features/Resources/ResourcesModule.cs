c1b584b2dec932b99cb167b96e1e001e1b9919f8
Exanite <exanite78012@gmail.com>
2024-06-10 08:30:55 -0400

    Remove Myra filesystem

--- Features/Resources/ResourcesModule.cs
+++ Features/Resources/ResourcesModule.cs
@@ -37,8 +37,6 @@ public class ResourcesModule : Module
 
                 resourceManager.Mount("Base:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Base")), true);
 
-                resourceManager.Mount("Myra:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Myra")), true);
-
                 resourceManager.Mount("Winter:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Winter", "Content")), true);
                 resourceManager.Mount("Base:", new DirectoryFileSystem(Path.Join(GameDirectories.ContentDirectory, "Winter", "Overrides", "Base")), true);
 
