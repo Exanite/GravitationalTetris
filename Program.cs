97b6552877b7be03146d029261e3c8b624ccd53b
William Chen <exanite78012@gmail.com>
2025-05-05 04:26:26 -0400

    Use EngineGameHost

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Autofac;
+﻿using System.Threading.Tasks;
 using Avalonia;
-using Exanite.Engine.Avalonia;
 using Exanite.Engine.Framework;
-using Exanite.Logging;
 
 namespace Exanite.GravitationalTetris;
 
@@ -14,38 +9,19 @@ public static class Program
     public const string CompanyName = "Exanite";
     public const string GameName = "GravitationalTetris";
 
-    public static async Task Main(string[] args)
-    {
-        var exitCode = 0;
-        {
-            Thread.CurrentThread.Name = "Main";
-
-            await using var game = CreateGame();
-            try
-            {
-                game.Initialize();
-                game.Run();
-            }
-            catch (Exception e)
-            {
-                LoggingUtility.LogProgramCrash(game.EngineSettings.Paths.LogsFolder, typeof(Program), e);
-
-                exitCode = 1;
-            }
-        }
-
-        Environment.Exit(exitCode);
-    }
-
-    private static EngineGame CreateGame()
+    public static readonly EngineGameHost Host = new(args =>
     {
         var settings = new EngineSettings(CompanyName, GameName);
         return new EngineGame(settings, [new GravitationalTetrisGameModule()]);
+    });
+
+    public static async Task Main(string[] args)
+    {
+        await Host.Run(args);
     }
 
     public static AppBuilder BuildAvaloniaApp()
     {
-        var game = CreateGame();
-        return game.Initialize().Resolve<AvaloniaContext>().Start(true);
+        return Host.BuildAvaloniaApp();
     }
 }
