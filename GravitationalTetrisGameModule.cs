291a0f29b7782f53132434c60bfbd026b4ef1b5e
William Chen <exanite78012@gmail.com>
2025-05-05 10:44:00 -0400

    Update code

--- GravitationalTetrisGameModule.cs
+++ GravitationalTetrisGameModule.cs
@@ -69,7 +69,7 @@ public class GravitationalTetrisGameModule : GameModule
 
         scheduler.DefaultGroup.RegisterSystem<WindowSystem>();
 
-        scheduler.DefaultGroup.RegisterSystem<EngineTimeSystem>();
+        scheduler.DefaultGroup.RegisterSystem<TimeSystem>();
         scheduler.DefaultGroup.RegisterSystem<InputSystem>();
         scheduler.DefaultGroup.RegisterSystem<InputActionSystem>();
 
