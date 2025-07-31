907734f4c01f06cf882a6700123696ab28e249d8
William Chen <exanite78012@gmail.com>
2025-07-31 00:46:06 -0400

    Disable FMOD code

--- Features/Audio/Systems/FmodAudioSystem.cs
+++ Features/Audio/Systems/FmodAudioSystem.cs
@@ -2,9 +2,6 @@ using System;
 using System.IO;
 using Exanite.Engine.Ecs.Systems;
 using Exanite.ResourceManagement;
-using FMOD.Studio;
-using Fmod = FMOD.System;
-using FmodStudio = FMOD.Studio.System;
 
 namespace Exanite.GravitationalTetris.Features.Audio.Systems;
 
@@ -15,9 +12,6 @@ public class FmodAudioSystem : GameSystem, ISetupSystem, IFrameUpdateSystem, IDi
     public const string ClearTile = "event:/ClearTile";
     public const string Restart = "event:/Restart";
 
-    private Fmod fmod;
-    private FmodStudio fmodStudio;
-
     private readonly ResourceManager resourceManager;
 
     public FmodAudioSystem(ResourceManager resourceManager)
@@ -27,49 +21,35 @@ public class FmodAudioSystem : GameSystem, ISetupSystem, IFrameUpdateSystem, IDi
 
     public void Setup()
     {
-        FmodStudio.create(out fmodStudio);
-        fmodStudio.getCoreSystem(out fmod);
-
-        fmodStudio.initialize(
-            maxchannels: 128,
-            studioflags: INITFLAGS.NORMAL,
-            flags: FMOD.INITFLAGS.NORMAL,
-            extradriverdata: nint.Zero
-        );
-
-        LoadBank("/Base/Tetris.bank");
-        LoadBank("/Base/Tetris.strings.bank");
+        // FmodStudio.create(out fmodStudio);
+        // fmodStudio.getCoreSystem(out fmod);
+        //
+        // fmodStudio.initialize(
+        //     maxchannels: 128,
+        //     studioflags: INITFLAGS.NORMAL,
+        //     flags: FMOD.INITFLAGS.NORMAL,
+        //     extradriverdata: nint.Zero
+        // );
+        //
+        // LoadBank("/Base/Tetris.bank");
+        // LoadBank("/Base/Tetris.strings.bank");
     }
 
     public void Play(string eventName)
     {
-        fmodStudio.getEvent(eventName, out var eventDescription);
-        eventDescription.createInstance(out var eventInstance);
-        eventInstance.start();
-        eventInstance.release();
+        // fmodStudio.getEvent(eventName, out var eventDescription);
+        // eventDescription.createInstance(out var eventInstance);
+        // eventInstance.start();
+        // eventInstance.release();
     }
 
     public void FrameUpdate()
     {
-        fmodStudio.update();
+        // fmodStudio.update();
     }
 
     public void Dispose()
     {
-        fmodStudio.unloadAll();
-    }
-
-    private Bank LoadBank(string resourceKey)
-    {
-        using (var stream = resourceManager.OpenFile(resourceKey))
-        using (var memoryStream = new MemoryStream())
-        {
-            stream.CopyTo(memoryStream);
-            var data = memoryStream.ToArray();
-
-            fmodStudio.loadBankMemory(data, LOAD_BANK_FLAGS.NORMAL, out var bank);
-
-            return bank;
-        }
+        // fmodStudio.unloadAll();
     }
 }
