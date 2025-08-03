fe38fb8f36b38c380056f35ab3dcc0f756d6e4ce
William Chen <exanite78012@gmail.com>
2025-08-03 12:40:45 -0400

    Draw tiles and tile placeholders

--- Features/Rendering/Systems/RendererSystem.cs
+++ Features/Rendering/Systems/RendererSystem.cs
@@ -1,14 +1,18 @@
 using System;
 using System.Numerics;
 using Exanite.Core.Runtime;
+using Exanite.Core.Utilities;
 using Exanite.Engine.Ecs.Queries;
 using Exanite.Engine.Ecs.Systems;
 using Exanite.Engine.Graphics;
 using Exanite.Engine.Graphics.Passes;
+using Exanite.Engine.Timing;
 using Exanite.Engine.Windowing;
 using Exanite.GravitationalTetris.Features.Cameras.Components;
 using Exanite.GravitationalTetris.Features.Sprites;
+using Exanite.GravitationalTetris.Features.Tetris.Components;
 using Exanite.GravitationalTetris.Features.Tetris.Systems;
+using Exanite.GravitationalTetris.Features.Tiles;
 using Exanite.ResourceManagement;
 using Silk.NET.Vulkan;
 
@@ -18,6 +22,9 @@ public partial class RendererSystem : GameSystem, ISetupSystem, IRenderSystem, I
 {
     public Texture2D WorldColor = null!;
     public Texture2D WorldDepth = null!;
+
+    private IResourceHandle<Texture2D> emptyTileTexture;
+    private IResourceHandle<Texture2D> placeholderTileTexture;
 
     private readonly SpriteBatcher spriteBatcher;
     private readonly CopyColorTexturePass copyWorldPass;
@@ -30,14 +37,28 @@ public partial class RendererSystem : GameSystem, ISetupSystem, IRenderSystem, I
     private readonly Window window;
     private readonly Swapchain swapchain;
     private readonly TetrisUiSystem tetrisUiSystem;
+    private readonly GameTilemapData tilemap;
+    private readonly ITime time;
 
-    public RendererSystem(GraphicsContext graphicsContext, ResourceManager resourceManager, Window window, Swapchain swapchain, TetrisUiSystem tetrisUiSystem)
+    public RendererSystem(
+        GraphicsContext graphicsContext,
+        ResourceManager resourceManager,
+        Window window,
+        Swapchain swapchain,
+        TetrisUiSystem tetrisUiSystem,
+        GameTilemapData tilemap,
+        ITime time)
     {
         this.graphicsContext = graphicsContext;
         this.resourceManager = resourceManager;
         this.window = window;
         this.swapchain = swapchain;
         this.tetrisUiSystem = tetrisUiSystem;
+        this.tilemap = tilemap;
+        this.time = time;
+
+        emptyTileTexture = resourceManager.GetResource(BaseMod.TileNone);
+        placeholderTileTexture = resourceManager.GetResource(BaseMod.TilePlaceholder);
 
         spriteBatcher = new SpriteBatcher(graphicsContext, resourceManager).AddTo(disposables);
 
@@ -150,9 +171,75 @@ public partial class RendererSystem : GameSystem, ISetupSystem, IRenderSystem, I
             batch.Draw(new SpriteInstanceDrawSettings()
             {
                 Texture = resourceManager.GetResource(BaseMod.Player).Value,
-                Model = Matrix4x4.CreateScale(1, -1, 1),
+                Model = Matrix4x4.Identity,
+            });
+
+            DrawTiles(batch);
+            DrawPlaceholdersQuery(World, batch);
+        }
+    }
+
+    private void DrawTiles(SpriteBatcher.Batch batch)
+    {
+        for (var x = 0; x < tilemap.Tiles.GetLength(0); x++)
+        {
+            for (var y = 0; y < tilemap.Tiles.GetLength(1); y++)
+            {
+                ref var tile = ref tilemap.Tiles[x, y];
+
+                var texture = emptyTileTexture;
+                if (tile.Shape != null)
+                {
+                    texture = tile.Shape.DefaultTexture;
+
+                    if (y + 1 == tilemap.Tiles.GetLength(1) || tilemap.Tiles[x, y + 1].Shape == null)
+                    {
+                        texture = tile.Shape.SnowTexture;
+                    }
+                }
+
+                var model = Matrix4x4.CreateTranslation(x, y, 0);
+
+                batch.Draw(new SpriteInstanceDrawSettings()
+                {
+                    Texture = texture.Value,
+                    Model = model,
+                });
+            }
+        }
+    }
+
+    [Query]
+    private void DrawPlaceholders([Data] SpriteBatcher.Batch batch, ref ComponentTetrisRoot tetrisRoot)
+    {
+        foreach (var blockPosition in tetrisRoot.PredictedBlockPositions)
+        {
+            var texture = placeholderTileTexture.Value;
+
+            var maxAlpha = 0.8f;
+            var minAlpha = 0.1f;
+            var alpha = MathUtility.Remap(EaseInOutCubic(time.Time / 1.5f), 0, 1, minAlpha, maxAlpha);
+
+            var model = Matrix4x4.CreateTranslation(blockPosition.X, blockPosition.Y, 0);
+
+            batch.Draw(new SpriteInstanceDrawSettings()
+            {
+                Texture = texture,
+                Model = model,
+                Color = new Vector4(1, 1, 1, alpha),
             });
         }
+    }
+
+    private float EaseInOutCubic(float t)
+    {
+        t = MathUtility.Wrap(t, 0, 2);
+        if (t > 1)
+        {
+            t = 2 - t;
+        }
+
+        return (float)(t < 0.5 ? 4 * t * t * t : 1 - Math.Pow(-2 * t + 2, 3) / 2);
     }
 
     public void Teardown()
