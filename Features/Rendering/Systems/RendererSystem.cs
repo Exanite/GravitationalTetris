bd981c87f1d55b4538aebb7b30e6b9e42a531479
William Chen <exanite78012@gmail.com>
2025-08-23 10:47:58 -0400

    Rename Resize to SetSize

--- Features/Rendering/Systems/RendererSystem.cs
+++ Features/Rendering/Systems/RendererSystem.cs
@@ -111,8 +111,8 @@ public partial class RendererSystem : GameSystem, IRenderSystem, IDisposable
         var commandBuffer = swapchain.CommandBuffer;
 
         // Resize world render targets
-        ActiveWorldColor.Resize(window.Size);
-        worldDepth.Resize(window.Size);
+        ActiveWorldColor.SetSize(window.Size);
+        worldDepth.SetSize(window.Size);
 
         // Clear world render targets
         clearPass.Clear(commandBuffer, [ActiveWorldColor], worldDepth);
