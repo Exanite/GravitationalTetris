e6e70f2792c43a3b17d5d7947820ccd10a95856d
William Chen <exanite78012@gmail.com>
2025-10-23 17:38:56 -0400

    Combine EngineGame and EngineGameHost classes into one class called EngineHost

--- Program.cs
+++ Program.cs
@@ -7,14 +7,9 @@ public static class Program
     public const string CompanyName = "Exanite";
     public const string GameName = "GravitationalTetris";
 
-    public static readonly EngineGameHost Host = new(args =>
-    {
-        var settings = new EngineSettings(CompanyName, GameName);
-        return new EngineGame(settings, [new GravitationalTetrisGameModule()]);
-    });
-
     public static void Main(string[] args)
     {
-        Host.Run(args);
+        var settings = new EngineSettings(CompanyName, GameName);
+        new EngineHost(settings, [new GravitationalTetrisGameModule()]).Launch();
     }
 }
