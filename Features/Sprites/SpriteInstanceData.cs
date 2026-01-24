c1fb30e839987f95b9dec32f885330f8346c8956
William Chen <exanite78012@gmail.com>
2026-01-24 14:20:00 -0500

    Update SpriteInstanceData to not implement IVertexBinding

--- Features/Sprites/SpriteInstanceData.cs
+++ Features/Sprites/SpriteInstanceData.cs
@@ -1,67 +1,11 @@
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Exanite.Engine.Graphics;
-using Silk.NET.Vulkan;
 
 namespace Exanite.GravitationalTetris.Features.Sprites;
 
 [StructLayout(LayoutKind.Sequential)]
-public struct SpriteInstanceData : IVertex
+public struct SpriteInstanceData
 {
-    public static InputLayoutDesc Layout { get; } = new()
-    {
-        Stride = Unsafe.SizeOf<SpriteInstanceData>(),
-        Elements =
-        [
-            // Model
-            new InputElementDesc()
-            {
-                ByteCount = 16,
-                Format = Format.R32G32B32A32Sfloat,
-            },
-            new InputElementDesc()
-            {
-                ByteCount = 16,
-                Format = Format.R32G32B32A32Sfloat,
-            },
-            new InputElementDesc()
-            {
-                ByteCount = 16,
-                Format = Format.R32G32B32A32Sfloat,
-            },
-            new InputElementDesc()
-            {
-                ByteCount = 16,
-                Format = Format.R32G32B32A32Sfloat,
-            },
-            // Color
-            new InputElementDesc()
-            {
-                ByteCount = 16,
-                Format = Format.R32G32B32A32Sfloat,
-            },
-            // UvOffset
-            new InputElementDesc()
-            {
-                ByteCount = 8,
-                Format = Format.R32G32Sfloat,
-            },
-            // UvSize
-            new InputElementDesc()
-            {
-                ByteCount = 8,
-                Format = Format.R32G32Sfloat,
-            },
-            // TextureIndex
-            new InputElementDesc()
-            {
-                ByteCount = 4,
-                Format = Format.R32Uint,
-            },
-        ],
-    };
-
     public Matrix4x4 Model;
 
     public Vector4 Color;
