fec7764a5154e1218fc2af60731a53b4621f417f
William Chen <exanite78012@gmail.com>
2026-06-29 11:23:00 -0400

    Rename GameName to ProgramName

--- Exanite.GravitationalTetris/Program.cs
+++ Exanite.GravitationalTetris/Program.cs
@@ -5,11 +5,11 @@ namespace Exanite.GravitationalTetris;
 public static class Program
 {
     public const string CompanyName = "Exanite";
-    public const string GameName = "GravitationalTetris";
+    public const string ProgramName = "GravitationalTetris";
 
     public static int Main(string[] args)
     {
-        var settings = new EngineSettings(CompanyName, GameName);
+        var settings = new EngineSettings(CompanyName, ProgramName);
         var engine = EngineRoot.Create(settings, [new GravitationalTetrisGameModule()]);
 
         return engine.Launch(args);
